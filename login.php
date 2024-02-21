d4b41432cffc1a31dab0377a09b60613ac6ec427
Juan Diego Valdivia Mendoza <107724321+JD17VM@users.noreply.github.com>
2024-02-21 10:40:21 -0500

    Update login.php 2lineas meta

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <?php
         $ruta_base_estilos = "";
         include("nav_footer/contenido_head.php");
