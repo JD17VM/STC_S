33c8d69fb3e493f766b8941efe74d71a20e1f4a6
Juan Diego Valdivia Mendoza <107724321+JD17VM@users.noreply.github.com>
2024-02-21 10:15:27 -0500

    Update conexion_bd.php online

--- conexion_bd.php
+++ conexion_bd.php
@@ -1,9 +1,17 @@
 <?php
   // Conectarse a la base de datos
-  $conexion = mysqli_connect("localhost", "root", "", "salud_tc");
+  $servername = "localhost";  // Reemplaza con el nombre del servidor de tu base de datos
+  $username = "c2081645_salud_t";       // Reemplaza con tu nombre de usuario de la base de datos
+  $password = "beze24siPA";           // Reemplaza con tu contraseña de la base de datos
+  $database = "c2081645_salud_t"; // Reemplaza con el nombre de tu base de datos
+
+  $conexion = new mysqli($servername, $username, $password, $database);
 
   // Verificar la conexión
   if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
   }
 ?>
+
+
+  
