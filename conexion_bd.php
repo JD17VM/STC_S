746d54152e7074aedc4cd5ae8f640056e5789d6f
Juan Diego Valdivia Mendoza <107724321+JD17VM@users.noreply.github.com>
2024-02-23 17:12:12 -0500

    Update conexion_bd.php corregir luego puse dos x

--- conexion_bd.php
+++ conexion_bd.php
@@ -2,7 +2,7 @@
   // Conectarse a la base de datos
   $servername = "localhost";  // Reemplaza con el nombre del servidor de tu base de datos
   $username = "c2081645_salud_t";       // Reemplaza con tu nombre de usuario de la base de datos
-  $password = "beze24siPA";           // Reemplaza con tu contraseña de la base de datos
+  $password = "xbeze24siPAx";           // Reemplaza con tu contraseña de la base de datos
   $database = "c2081645_salud_t"; // Reemplaza con el nombre de tu base de datos
 
   $conexion = new mysqli($servername, $username, $password, $database);
