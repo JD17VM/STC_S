e8cd34f99a39e5c3b9089a0c1a5b9e1aec094375
KodingT <kodingtutoriales@gmail.com>
2024-02-25 15:33:03 -0500

    w

--- conexion_bd.php
+++ conexion_bd.php
@@ -1,9 +1,9 @@
 <?php
   // Conectarse a la base de datos
-  $servername = "localhost";  // Reemplaza con el nombre del servidor de tu base de datos
-  $username = "c2081645_salud_t";       // Reemplaza con tu nombre de usuario de la base de datos
-  $password = "beze24siPA";           // Reemplaza con tu contraseña de la base de datos
-  $database = "c2081645_salud_t"; // Reemplaza con el nombre de tu base de datos
+  $servername = "localhost";  
+  $username = "c2081645_salud_t";       
+  $password = "beze24siPA";           
+  $database = "c2081645_salud_t"; 
 
   $conexion = new mysqli($servername, $username, $password, $database);
   // $conexion = mysqli_connect("localhost", "root", "", "salud_tc");
